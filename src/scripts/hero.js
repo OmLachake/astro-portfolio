1dea325c66b56f321429a8af24ca0458ad8e05a4
Om Lachake <omlachake@pm.me>
2026-02-04 18:32:07 +0000

    added fix for tablet sizes

--- src/scripts/hero.js
+++ src/scripts/hero.js
@@ -1,6 +1,8 @@
 import gsap from 'gsap';
+import ScrollTrigger from 'gsap/ScrollTrigger';
+
 const wave = document.querySelectorAll('.wave');
-const nextSection = document.querySelectorAll('.next-section');
+const nextSection = document.querySelector('.next-section');
 const heroSection = document.querySelector('.hero');
 const bgEffect = document.querySelector('.hero-bg-effect');
 
@@ -10,7 +12,16 @@ if (window.lenis) {
 
 let mm = gsap.matchMedia();
 
-mm.add('(min-width: 800px)', () => {
+let scrollTimeout;
+window.addEventListener('scroll', () => {
+    clearTimeout(scrollTimeout);
+
+    scrollTimeout = setTimeout(() => {
+        ScrollTrigger.refresh();
+    }, 150);
+});
+
+mm.add('(min-width: 1200px)', () => {
     gsap.from(wave, {
         duration: 1,
         opacity: 0,
@@ -35,6 +46,65 @@ mm.add('(min-width: 800px)', () => {
                 ease: 'none',
             },
         );
+    });
+
+    gsap.from('.hero-sub-title', {
+        y: 50,
+        opacity: 0,
+        duration: 1,
+        ease: 'expo.out',
+        delay: 0.25,
+    });
+
+    gsap.from(bgEffect, {
+        y: 50,
+        scale: 1.15,
+        opacity: 0,
+        duration: 1,
+        ease: 'expo.out',
+        delay: 0.0,
+    });
+
+    gsap.from('.title.left', {
+        x: -50,
+        opacity: 0,
+        duration: 1,
+        ease: 'expo.out',
+        delay: 0.15,
+    });
+
+    gsap.from('.title.right', {
+        x: 50,
+        opacity: 0,
+        duration: 1,
+        ease: 'expo.out',
+        delay: 0.15,
+    });
+});
+
+mm.add('(min-width: 800px) and (max-width: 1200px)', () => {
+    gsap.from(wave, {
+        duration: 1,
+        opacity: 0,
+        y: (i) => 500 + i * 100,
+        stagger: 0.05,
+        ease: 'expo.out',
+    });
+
+    wave.forEach((w, i) => {
+        console.log('hi');
+        gsap.to(w, {
+            y: `-=${(1 / (i + 1)) * 180}`,
+            scrollTrigger: {
+                trigger: nextSection,
+                start: 'top bottom',
+                end: 'bottom bottom',
+                scrub: 1.2 + i * 0.2,
+                invalidateOnRefresh: true,
+            },
+            ease: 'none',
+            immediateRender: false,
+        });
     });
 
     gsap.from('.hero-sub-title', {
